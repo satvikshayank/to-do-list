b654ce059f73818bdac8a8aa5bff451cd0e6a783
satvikshayank <65305922+satvikshayank@users.noreply.github.com>
2023-08-17 23:30:00 +0530

    Add files via upload

---
+++ daily-to-do-list/script.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container")
+
+
+function addtask(){
+
+    if(inputBox.value == " ")
+    {
+        alert("you must write something");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
